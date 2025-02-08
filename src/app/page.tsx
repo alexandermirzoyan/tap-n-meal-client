96289767d2a28a49c89cb8d0a02da627742f0f22
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2025-02-08 16:20:23 +0400

    feat: rendering Typography and Button components at Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+import { Button } from '@/components/Button';
+import { Typography } from '@/components/Typography';
+
 const Home = () => (
-  <p>Hello World!</p>
+  <>
+    <Typography size='xxl'>Hello World!</Typography>
+    <Typography size='xl'>Hello World!</Typography>
+    <Typography size='lg'>Hello World!</Typography>
+    <Typography size='md'>Hello World!</Typography>
+    <Typography size='sm'>Hello World!</Typography>
+    <Typography size='xs'>Hello World!</Typography>
+    <Typography size='xxs'>Hello World!</Typography>
+
+    <Button label='Add to Card' />
+  </>
 );
 
 export default Home;
