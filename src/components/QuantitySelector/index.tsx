74746b5c2b2ed839c9d5e7d55605ee24f156151b
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2025-02-11 20:18:00 +0400

    feat: <QuantitySelector /> component created

---
+++ src/components/QuantitySelector/index.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { useState } from 'react';
+import { Typography } from '@/components/Typography';
+
+import { IQuantitySelectorProps } from './types';
+
+import MinusIcon from '../../../public/icons/minus.svg';
+import PlusIcon from '../../../public/icons/plus.svg';
+
+import './styles.scss';
+
+export const QuantitySelector = ({ max }: IQuantitySelectorProps) => {
+  const min = 1;
+  const [count, setCount] = useState(1);
+
+  const onChangeClick = (type: 'plus' | 'minus') => {
+    if (type === 'minus' && count > min) {
+      setCount(count - 1);
+      return;
+    }
+
+    if (type === 'plus' && count < max) {
+      setCount(count + 1);
+    }
+  };
+
+  return (
+    <div className='quantity-selector'>
+      <button onClick={() => onChangeClick('minus')} disabled={count === min}>
+        <MinusIcon />
+      </button>
+      <Typography>{count}</Typography>
+      <button onClick={() => onChangeClick('plus')} disabled={count === max}>
+        <PlusIcon />
+      </button>
+    </div>
+  );
+};
---
+++ src/components/QuantitySelector/types.ts
@@ -0,0 +1,4 @@
+export interface IQuantitySelectorProps {
+  currentQuantity?: number;
+  max: number;
+}
