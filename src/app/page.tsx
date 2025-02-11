4914726e7012b3d98cb5310b44540590d681a87f
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2025-02-11 20:18:23 +0400

    feat: testing QuantitySelector component's UI at homepage with rendering in a white background to see real color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/Button';
 import { Typography } from '@/components/Typography';
+import { QuantitySelector } from '@/components/QuantitySelector';
 
 import CartIcon from '../../public/icons/cart.svg';
 
@@ -16,6 +17,10 @@ const Home = () => (
     <Button label='Add to Card' prefix={<CartIcon />} />
     <Button label='Add to Card' isLoading prefix={<CartIcon />} />
     <Button label='Add to Card' disabled prefix={<CartIcon />} />
+
+    <div style={{ display: 'inline-block', backgroundColor: 'white', padding: 20 }}>
+      <QuantitySelector max={10} />
+    </div>
   </>
 );
 
