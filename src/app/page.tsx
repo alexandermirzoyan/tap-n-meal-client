e134bcb26f21420866ee75577b01592b09022752
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2025-02-13 23:15:05 +0400

    feat: testing Modal component's UI at Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,67 @@
 'use client';
 
+import { useState } from 'react';
+
 import { Button } from '@/components/Button';
 import { Typography } from '@/components/Typography';
 import { QuantitySelector } from '@/components/QuantitySelector';
 import { ProductCard } from '@/components/ProductCard';
 import { BackButton } from '@/components/BackButton';
 import { Radio } from '@/components/Radio';
+import { Modal } from '@/components/Modal';
 
 import CartIcon from '../../public/icons/cart.svg';
 
-const Home = () => (
-  <>
-    <Typography size='xxl'>Hello World!</Typography>
-    <Typography size='xl'>Hello World!</Typography>
-    <Typography size='lg'>Hello World!</Typography>
-    <Typography size='md'>Hello World!</Typography>
-    <Typography size='sm'>Hello World!</Typography>
-    <Typography size='xs'>Hello World!</Typography>
-    <Typography size='xxs'>Hello World!</Typography>
+const Home = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
 
-    <Button label='Add to Card' prefix={<CartIcon />} />
-    <Button label='Add to Card' isLoading prefix={<CartIcon />} />
-    <Button label='Add to Card' disabled prefix={<CartIcon />} />
+  return (
+    <>
+      <Typography size='xxl'>Hello World!</Typography>
+      <Typography size='xl'>Hello World!</Typography>
+      <Typography size='lg'>Hello World!</Typography>
+      <Typography size='md'>Hello World!</Typography>
+      <Typography size='sm'>Hello World!</Typography>
+      <Typography size='xs'>Hello World!</Typography>
+      <Typography size='xxs'>Hello World!</Typography>
 
-    <div style={{ display: 'inline-block', backgroundColor: 'white', padding: 20 }}>
-      <QuantitySelector max={10} />
-    </div>
+      <Button label='OPEN MODAL' onClick={() => setModalOpen(true)} />
+      <Button label='Add to Card' prefix={<CartIcon />} />
+      <Button label='Add to Card' isLoading prefix={<CartIcon />} />
+      <Button label='Add to Card' disabled prefix={<CartIcon />} />
 
-    <div style={{ display: 'inline-block', backgroundColor: '#2C2C2C', padding: 20 }}>
-      <ProductCard title='Veggie tomato mix' available={1} price={30} />
-      <ProductCard title='Veggie tomato mix' available={1} price={30} isVeggie />
-      <ProductCard title='Turur' available={30} price={100} />
-    </div>
+      <div style={{ display: 'inline-block', backgroundColor: 'white', padding: 20 }}>
+        <QuantitySelector max={10} />
+      </div>
 
-    <BackButton to='/' />
+      <div style={{ display: 'inline-block', backgroundColor: '#2C2C2C', padding: 20 }}>
+        <ProductCard title='Veggie tomato mix' available={1} price={30} />
+        <ProductCard title='Veggie tomato mix' available={1} price={30} isVeggie />
+        <ProductCard title='Turur' available={30} price={100} />
+      </div>
 
-    <Radio
-      label='Payment Method'
-      onChange={(value: string) => console.log(value)}
-      options={[
-        { id: '1', name: 'Credit Card' },
-        { id: '2', name: 'Cash' },
-      ]}
-    />
+      <BackButton to='/' />
 
-    <div style={{ marginBottom: 200 }} />
-  </>
-);
+      <Radio
+        label='Payment Method'
+        onChange={(value: string) => console.log(value)}
+        options={[
+          { id: '1', name: 'Credit Card' },
+          { id: '2', name: 'Cash' },
+        ]}
+      />
+
+      <Modal
+        open={isModalOpen}
+        onClose={() => setModalOpen(false)}
+        type='error'
+        title='Oops...'
+        info='Something went wrong. Try again'
+      />
+
+      <div style={{ marginBottom: 200 }} />
+    </>
+  );
+};
 
 export default Home;
