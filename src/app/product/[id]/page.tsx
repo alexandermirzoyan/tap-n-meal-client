436f2636cf2ac00ef750cf07119ac5cbc1a26ca0
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2025-02-19 23:01:24 +0400

    feat: product description page image UI with blurred background created

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,4 +1,10 @@
+import Image from 'next/image';
+
 import { Typography } from '@/components/Typography';
+import { BackButton } from '@/components/BackButton';
+import { getImageSrc } from '@/utils/getImageSrc';
+
+import './styles.scss';
 
 interface IPageProps {
   params: Promise<{ id: string }>;
@@ -6,9 +12,22 @@ interface IPageProps {
 
 const ProductPage = async ({ params }: IPageProps) => {
   const id = (await params).id;
+  const imageSrc = '/uploads/product-mock-1.png';
 
   return (
-    <Typography>{`Product #${id}`}</Typography>
+    <>
+      <div className='product--image-container'>
+        <div className='product--image-blur' style={{ backgroundImage: `url(${getImageSrc(imageSrc)})` }} />
+        <BackButton to='/menu/12' />
+        <Image
+          width={220}
+          height={220}
+          src={getImageSrc(imageSrc)}
+          alt='Product Image'
+        />
+      </div>
+      <Typography>{`Product #${id}`}</Typography>
+    </>
   );
 };
 
