6f7b214ab1a80d838921b80d0ad85640314239f6
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2025-03-01 18:14:39 +0400

    feat: tip radio removed from checkout page, added disabled state for button

--- src/app/checkout/[id]/page.tsx
+++ src/app/checkout/[id]/page.tsx
@@ -5,9 +5,7 @@ import { useState } from 'react';
 import { HeaderAction } from '@/components/HeaderAction';
 import { Typography } from '@/components/Typography';
 import { Radio } from '@/components/Radio';
-import { Input } from '@/components/Input';
 import { Button } from '@/components/Button';
-import { formatNumber } from '@/utils/formatNumber';
 
 import './styles.scss';
 
@@ -16,20 +14,11 @@ const PAYMENT_METHODS = [
   { id: 'cash', name: 'Cash' },
 ];
 
-const TIP_OPTIONS = [
-  { id: '1', name: 'No' },
-  { id: '2', name: `${formatNumber(500)} ֏` },
-  { id: '3', name: `${formatNumber(1000)} ֏` },
-  { id: 'other', name: 'Other amount' },
-];
-
 const CheckoutPage = () => {
   const tableId = 12;
   const [selectedMethod, setSelectedMethod] = useState<string | null>(null);
-  const [tip, setTip] = useState<string | null>(null);
 
   console.log('selectedMethod :: ', selectedMethod);
-  console.log('tip :: ', tip);
 
   return (
     <>
@@ -41,18 +30,7 @@ const CheckoutPage = () => {
         className='checkout--method-input'
         onChange={(id: string) => setSelectedMethod(id)}
       />
-      <Radio
-        label='Do you want to leave a tip?'
-        options={TIP_OPTIONS}
-        onChange={(id: string) => setTip(id)}
-      />
-      {tip === 'other' ? (
-        <div className='checkout--amount-container'>
-          <Typography weight='medium' size='xs'>Amount</Typography>
-          <Input placeholder='Enter the amount' type='number' />
-        </div>
-      ) : null}
-      <Button label='Checkout' className='fixed-action-btn' />
+      <Button label='Checkout' disabled={!selectedMethod} className='fixed-action-btn' />
       <div className='checkout--blank-area' />
     </>
   );
