c24649a833d9e7f1e581a1470846cb6ed157457c
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2025-03-04 23:08:03 +0400

    feat: rendering success modal at menu page when getting redirected from checkout

---
+++ src/app/menu/[table]/_components/SuccessModal/index.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import { Modal } from '@/components/Modal';
+
+import { ISuccessModalProps } from './types';
+
+export const SuccessModal = ({ open }: ISuccessModalProps) => {
+  const tableId = 12;
+  const router = useRouter();
+
+  if (!open) {
+    return null;
+  }
+
+  const onModalClose = () => {
+    router.push(`/menu/${tableId}`);
+  };
+
+  return (
+    <Modal
+      open
+      type='success'
+      onClose={onModalClose}
+      title='Thank you for your order'
+      info='Your order was successfully done'
+    />
+  );
+};
--- src/app/menu/[table]/page.tsx
+++ src/app/menu/[table]/page.tsx
@@ -6,6 +6,7 @@ import { Input } from '@/components/Input';
 
 import { Categories } from './_components/Categories';
 import { Products } from './_components/Products';
+import { SuccessModal } from './_components/SuccessModal';
 
 import LogoIcon from '../../../../public/icons/logo.svg';
 import SearchIcon from '../../../../public/icons/magnifier.svg';
@@ -14,10 +15,12 @@ import './styles.scss';
 
 interface IPageProps {
   params: Promise<{ table: string }>;
+  searchParams: Promise<{ success?: string }>;
 }
 
-const MenuPage = async ({ params }: IPageProps) => {
+const MenuPage = async ({ params, searchParams }: IPageProps) => {
   const table = (await params).table;
+  const showSuccess = (await searchParams).success !== undefined;
   const categories = await request({ url: '/categories' });
   console.log(`Menu for table ${table}`);
 
@@ -36,6 +39,7 @@ const MenuPage = async ({ params }: IPageProps) => {
       </div>
 
       <Products />
+      <SuccessModal open={showSuccess} />
     </>
   );
 };
