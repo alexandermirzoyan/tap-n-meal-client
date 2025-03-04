df2b549aae1cb0f0ee45663eac5c07ce9f7a1c0b
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2025-03-04 23:02:07 +0400

    feat: request() service function added support to send request with specific HTTP method

--- src/service/request.ts
+++ src/service/request.ts
@@ -2,11 +2,21 @@ import { APP_LANGUAGE } from '@/constants/appLanguage';
 
 type TRequestParams = {
   url: string;
+  method?: 'GET' | 'POST' | 'PUT' | 'DELETE';
+  body?: any;
 }
 
-export const request = async ({ url }: TRequestParams) => {
+export const request = async ({ url, method = 'GET', body }: TRequestParams) => {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_ORIGIN}${url}`, { headers: { Language: APP_LANGUAGE } });
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_ORIGIN}${url}`, {
+      method,
+      body: body ? JSON.stringify(body) : undefined,
+      headers: {
+        Language: APP_LANGUAGE,
+        'Content-Type': 'application/json',
+      },
+    });
+
     return response.json();
   } catch (error) {
     console.error(error);
