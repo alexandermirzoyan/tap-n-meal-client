c7012ef58c1a62c1b019a15642ced2f0a315647b
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2025-03-05 20:44:49 +0400

    feat: useOrder() hook created

---
+++ src/hooks/useOrder.ts
@@ -0,0 +1,31 @@
+import { use } from 'react';
+import { useRouter } from 'next/navigation';
+
+import { request } from '@/service/request';
+import { CartContext } from '@/context/Cart';
+
+export const useOrder = () => {
+  const tableId = 12;
+  const { products, clearProducts } = use(CartContext);
+  const router = useRouter();
+
+  const createOrder = async (paymentMethod: string) => {
+    const orderProducts = products.map((product) => ({ id: product.id, quantity: product.count, comment: product.comment }));
+    const response = await request({
+      url: '/orders',
+      method: 'POST',
+      body: {
+        paymentMethod,
+        table: tableId,
+        products: orderProducts,
+      },
+    });
+
+    if (response.success) {
+      clearProducts();
+      router.push(`/menu/${tableId}?success`);
+    }
+  };
+
+  return { createOrder };
+};
