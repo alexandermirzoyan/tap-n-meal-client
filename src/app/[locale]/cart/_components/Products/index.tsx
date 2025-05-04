ea3ca07d5ba37dcfeed9d5a795c426924e8fd010
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2025-05-04 15:38:49 +0400

    feat: cart page data localized

--- src/app/[locale]/cart/_components/Products/index.tsx
+++ src/app/[locale]/cart/_components/Products/index.tsx
@@ -1,22 +1,47 @@
 'use client';
 
-import { use } from 'react';
+import { use, useState, useEffect } from 'react';
 import Image from 'next/image';
 
 import { CartContext } from '@/context/Cart';
+import { ICartProduct } from '@/context/Cart/types';
 import { Typography } from '@/components/Typography';
 import { QuantitySelector } from '@/components/QuantitySelector';
 import { getImageSrc } from '@/utils/getImageSrc';
 import { formatNumber } from '@/utils/formatNumber';
+import { getLanguage } from '@/utils/getLanguage';
+import { request } from '@/service/request';
 
 import TrashIcon from '../../../../../../public/icons/trash.svg';
 
 import './styles.scss';
 
 export const Products = () => {
+  const [localizedProducts, setLocalizedProducts] = useState<ICartProduct[]>([]);
   const { products, removeProduct } = use(CartContext);
 
-  return products.map((product) => (
+  const loadProducts = async () => {
+    const language = await getLanguage();
+    const localizedProductsToSet: ICartProduct[] = [];
+
+    for (const product of products) {
+      const response = await request({
+        url: `/products/${product.id}`,
+        headers: { Authorization: 'Bearer TOKEN', Language: '' },
+      });
+
+      const foundNameInLocale = response.name.find((name: { locale: { code: string } }) => name.locale.code === language);
+      localizedProductsToSet.push({ ...product, name: foundNameInLocale.name });
+    }
+
+    setLocalizedProducts(localizedProductsToSet);
+  };
+
+  useEffect(() => {
+    loadProducts();
+  }, [products]);
+
+  return localizedProducts.map((product) => (
     <div key={product.id} className='product-cart--container'>
       <div className='product-cart--image-container'>
         <Image src={getImageSrc(product.image.path)} width={100} height={100} alt='Image' />
