6a24bf53b97f4b751087315edc76129bfd55639a
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2025-05-04 16:06:20 +0400

    feat: showing total price at OrderButton on Cart page

--- src/app/[locale]/cart/_components/OrderButton/index.tsx
+++ src/app/[locale]/cart/_components/OrderButton/index.tsx
@@ -5,20 +5,23 @@ import { useTranslations } from 'next-intl';
 
 import { Button } from '@/components/Button';
 import { CartContext } from '@/context/Cart';
+import { formatNumber } from '@/utils/formatNumber';
 import { Link } from '@/i18n/navigation';
 
 export const OrderButton = () => {
   const tableId = 12;
-  const { count } = use(CartContext);
+  const { count, products } = use(CartContext);
   const t = useTranslations();
 
   if (!count) {
     return null;
   }
 
+  const totalPrice = products.reduce((acc, cur) => acc + cur.price * cur.count, 0);
+
   return (
     <Link href={`/checkout/${tableId}`}>
-      <Button label={t('placeOrder')} className='fixed-action-btn' />
+      <Button label={`${t('placeOrder')} - ${formatNumber(totalPrice)} ֏`} className='fixed-action-btn' />
     </Link>
   );
 };
